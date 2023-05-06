81c37890857b3fd5bb8515b1823ca3bf5b1df3ae
Srylax <a5910f11@srylax.dev>
2023-05-06 04:06:54 +0200

    Added clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,23 @@
 #![doc = include_str!("../README.md")]
+#![forbid(unsafe_code)]
+#![warn(
+    clippy::cast_lossless,
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_precision_loss,
+    clippy::cast_sign_loss,
+    clippy::checked_conversions,
+    clippy::implicit_saturating_sub,
+    clippy::integer_arithmetic,
+    clippy::mod_module_files,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::unwrap_used,
+    missing_docs,
+    rust_2018_idioms,
+    unused_lifetimes,
+    unused_qualifications
+)]
 
 //! ### Usage:
 //! The usage is a bit different than the node version. See the examples for more details and a working example.
